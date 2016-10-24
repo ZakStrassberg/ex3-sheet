e654cc6648138265e237ddbf9502cf6ee7c26bc3
Zak Strassberg <zakstrassberg@gmail.com>
2016-10-23 22:59:09 -0700

    initial commit

---
+++ public/javascripts/app.js
@@ -0,0 +1,20 @@
+Vue.config.devtools = true
+var app;
+app = new Vue({
+  el: '#app',
+  data: {
+    attributes: {
+      strength: 1,
+      dexterity: 0,
+      stamina: 0,
+      intelligence: 0,
+      perception: 0,
+      wits: 0,
+      charisma: 0,
+      manipulation: 0,
+      appearance: 0
+    }
+  },
+  methods: {},
+  computed: {}
+})
